7e321b3db4f1fb12d7a3c4c23a62e5caa4a62ca4
peng <1471947564@qq.com>
2019-08-27 16:27:39 +0800

    Site updated: 2019-08-27 16:27:38

--- aplayer/music.js
+++ aplayer/music.js
@@ -2,6 +2,7 @@ const ap = new APlayer({
   container: document.getElementById('aplayer'),
   fixed: true,
   autoplay: true,
+  lrcType: 3,
   audio: [{
       name: "等你下课",
       artist: '周杰伦',
@@ -14,12 +15,14 @@ const ap = new APlayer({
       artist: '周杰伦',
       url: './aplayer/mp3/告白气球_周杰伦_128K.mp3',
       cover: './aplayer/mp3/周杰伦的床边故事.jpg',
+      lrc: './aplayer/mp3/告白气球_周杰伦_128K.lrc',
     },
     {
       name: "兰亭序",
       artist: '周杰伦',
       url: './aplayer/mp3/兰亭序_周杰伦_128K.mp3',
       cover: './aplayer/mp3/摩杰座.jpg',
+      lrc: './aplayer/mp3/兰亭序_周杰伦_128K.lrc',
     }
   ]
 });
