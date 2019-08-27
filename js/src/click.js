9b21e203eed4cf3bac60c067e8c44cce212d3b87
peng <1471947564@qq.com>
2019-08-27 11:47:58 +0800

    Site updated: 2019-08-27 11:47:56

--- js/src/click.js
+++ js/src/click.js
@@ -1,1 +1,157 @@
-onload=function(){var c=0,e=document.getElementsByTagName("html")[0],i=document.getElementsByTagName("body")[0];e.onclick=function(e){var n=document.createElement("b");n.style.color="#E94F06",n.style.zIndex=9999,n.style.position="absolute",n.style.select="none";var a=e.pageX,r=e.pageY;switch(n.style.left=a-10+"px",n.style.top=r-20+"px",clearInterval(t),++c){case 2:n.innerText="😀";break;case 3:n.innerText="😃";break;case 4:n.innerText="❤";break;case 5:n.innerText="😁";break;case 6:n.innerText="😆";break;case 7:n.innerText="😅";break;case 8:n.innerText="😂";break;case 9:n.innerText="🤣";break;case 10:n.innerText="😊";break;case 11:n.innerText="😚";break;case 12:n.innerText="😙";break;case 13:n.innerText="😗";break;case 14:n.innerText="😘";break;case 15:n.innerText="😍";break;case 16:n.innerText="😌";break;case 17:n.innerText="😉";break;case 18:n.innerText="🙃";break;case 19:n.innerText="🙂";break;case 20:n.innerText="😥";break;case 21:n.innerText="😋";break;case 22:n.innerText="😜";break;case 23:n.innerText="😝";break;case 24:n.innerText="😛";break;case 25:n.innerText="😨";break;case 26:n.innerText="😒";break;case 27:n.innerText="😎";break;case 28:n.innerText="🙄";break;case 29:n.innerText="😭";break;case 30:n.innerText="😖";break;case 31:n.innerText="(๑•́ ∀ •̀๑)";break;case 32:n.innerText="(๑•́ ₃ •̀๑)";break;case 40:n.innerText="(๑•̀_•́๑)";break;case 50:n.innerText="（￣へ￣）";break;case 60:n.innerText="(╯°口°)╯(┴—┴";break;case 70:n.innerText="૮( ᵒ̌皿ᵒ̌ )ა";break;case 80:n.innerText="╮(｡>口<｡)╭";break;case 90:n.innerText="( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃";break;case 100:case 101:case 102:case 103:case 104:n.innerText="😏";break;case 105:n.innerText="(ꐦ°᷄д°᷅)";break;default:n.innerText="😂"}n.style.fontSize=10*Math.random()+8+"px";var t,s=0;setTimeout(function(){t=setInterval(function(){150==++s&&(clearInterval(t),i.removeChild(n)),n.style.top=r-20-s+"px",n.style.opacity=(150-s)/120},8)},70),i.appendChild(n)}};
+onload = function () {
+  var click_cnt = 0;
+  var $html = document.getElementsByTagName("html")[0];
+  var $body = document.getElementsByTagName("body")[0];
+  $html.onclick = function (e) {
+    var $elem = document.createElement("b");
+    $elem.style.color = "#E94F06";
+    $elem.style.zIndex = 9999;
+    $elem.style.position = "absolute";
+    $elem.style.select = "none";
+    var x = e.pageX;
+    var y = e.pageY;
+    $elem.style.left = (x - 10) + "px";
+    $elem.style.top = (y - 20) + "px";
+    clearInterval(anim);
+    switch (++click_cnt) {
+      case 2:
+        $elem.innerText = "😀";
+        break;
+      case 3:
+        $elem.innerText = "😃";
+        break;
+      case 4:
+        $elem.innerText = "❤";
+        break;
+      case 5:
+        $elem.innerText = "😁";
+        break;
+      case 6:
+        $elem.innerText = "😆";
+        break;
+      case 7:
+        $elem.innerText = "😅";
+        break;
+      case 8:
+        $elem.innerText = "😂";
+        break;
+      case 9:
+        $elem.innerText = "🤣";
+        break;
+      case 10:
+        $elem.innerText = "😊";
+        break;
+      case 11:
+        $elem.innerText = "😚";
+        break;
+      case 12:
+        $elem.innerText = "😙";
+        break;
+      case 13:
+        $elem.innerText = "😗";
+        break;
+      case 14:
+        $elem.innerText = "😘";
+        break;
+      case 15:
+        $elem.innerText = "😍";
+        break;
+      case 16:
+        $elem.innerText = "😌";
+        break;
+      case 17:
+        $elem.innerText = "😉";
+        break;
+      case 18:
+        $elem.innerText = "🙃";
+        break;
+      case 19:
+        $elem.innerText = "🙂";
+        break;
+      case 20:
+        $elem.innerText = "😥";
+        break;
+      case 21:
+        $elem.innerText = "😋";
+        break;
+      case 22:
+        $elem.innerText = "😜";
+        break;
+      case 23:
+        $elem.innerText = "😝";
+        break;
+      case 24:
+        $elem.innerText = "😛";
+        break;
+      case 25:
+        $elem.innerText = "😨";
+        break;
+      case 26:
+        $elem.innerText = "😒";
+        break;
+      case 27:
+        $elem.innerText = "😎";
+        break;
+      case 28:
+        $elem.innerText = "🙄";
+        break;
+      case 29:
+        $elem.innerText = "😭";
+        break;
+      case 30:
+        $elem.innerText = "😖";
+        break;
+      case 31:
+        $elem.innerText = "(๑•́ ∀ •̀๑)";
+        break;
+      case 32:
+        $elem.innerText = "(๑•́ ₃ •̀๑)";
+        break;
+      case 40:
+        $elem.innerText = "(๑•̀_•́๑)";
+        break;
+      case 50:
+        $elem.innerText = "（￣へ￣）";
+        break;
+      case 60:
+        $elem.innerText = "(╯°口°)╯(┴—┴";
+        break;
+      case 70:
+        $elem.innerText = "૮( ᵒ̌皿ᵒ̌ )ა";
+        break;
+      case 80:
+        $elem.innerText = "╮(｡>口<｡)╭";
+        break;
+      case 90:
+        $elem.innerText = "( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃";
+        break;
+      case 100:
+      case 101:
+      case 102:
+      case 103:
+      case 104:
+        $elem.innerText = "😏";
+        break;
+      case 105:
+        $elem.innerText = "(ꐦ°᷄д°᷅)";
+        break;
+      default:
+        $elem.innerText = "😂";
+        break;
+    }
+    $elem.style.fontSize = Math.random() * 10 + 8 + "px";
+    var increase = 0;
+    var anim;
+    setTimeout(function () {
+      anim = setInterval(function () {
+        if (++increase == 150) {
+          clearInterval(anim);
+          $body.removeChild($elem);
+        }
+        $elem.style.top = y - 20 - increase + "px";
+        $elem.style.opacity = (150 - increase) / 120;
+      }, 8);
+    }, 70);
+    $body.appendChild($elem);
+  };
+};
